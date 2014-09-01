e91ebf489a506f68941a368e127d2f0b010e5872
Amanda Folson <afolson@users.noreply.github.com>
2014-08-31 20:00:27 -0700

    RELEASE THE KRAKEN aka 1.0.0.

--- greentext.pl
+++ greentext.pl
@@ -4,7 +4,7 @@ use strict;
 use Irssi;
 
 my %IRSSI = (
-  version   => '0.1',
+  version   => '1.0.0',
   author    => 'Amanda Folson',
   contact   => 'amanda@incredibl.org',
   name    => 'irssi-greentext',
@@ -13,14 +13,36 @@ my %IRSSI = (
   license   => 'GPL',
 );
 
+# Look for >messages sent to us/from us
 sub greentext {
-  my ($msg, $server, $witem) = @_;
-  if ($msg =~ /^\>\s*/) {
-    Irssi::signal_stop();
-    Irssi::signal_remove('send text', 'greentext');
-    Irssi::signal_emit('send text', "\x{03}3$msg", $server, $witem);
-    Irssi::signal_add('send text', 'greentext');
+  my $sig = Irssi::signal_get_emitted();
+  # Crappy workaround to parse these since public/private have different args :(
+  if ($sig eq "message public") {
+    my ($server, $msg, $nick, $address, $target) = @_;
+    if ($msg =~ /^\>\s*/) {
+      Irssi::signal_emit("message public", $server, "\x{03}3$msg", $nick, $address, $target);
+      Irssi::signal_stop();
+    }
+  }
+  if ($sig eq "message private") {
+    my ($server, $msg, $nick, $address) = @_;
+    if ($msg =~ /^\>\s*/) {
+      Irssi::signal_emit("message private", $server, "\x{03}3$msg", $nick, $address);
+      Irssi::signal_stop();
+    }
+  }
+  # We sent something!
+  if ($sig eq "send text") {
+    my ($msg, $server, $witem) = @_;
+    if ($msg =~ /^\>\s*/) {
+      Irssi::signal_emit('send text', "\x{03}3$msg", $server, $witem);
+      Irssi::signal_stop();
+    }
   }
 }
 
+# Public and private messages sent to us
+Irssi::signal_add('message public', 'greentext');
+Irssi::signal_add('message private', 'greentext');
+# Public and private messages sent from us
 Irssi::signal_add('send text', 'greentext');
